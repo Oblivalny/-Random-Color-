218c9ef5c708644ef3f74156c4e09442a7c086c3
днс <@DESKTOP-P6D9CRD>
2017-02-23 12:05:09 +0500

    Добавьте файлы проекта.

---
+++ DAY_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_2
+{
+    class Program
+    {
+
+        static int[] Заполнение_массива(int[] arr)//Рандомное заполение массива !
+        {
+            Random x = new Random();
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                arr[i] = x.Next(100);
+            }
+            return arr;
+        }
+
+        static void Вывод_массива(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Элимент №{0}: {1}",i,arr[i]);
+            }
+
+        }
+        
+        static void Main(string[] args)
+        {
+
+            
+
+            int n = 0 ;
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+
+            Заполнение_массива(arr);
+            Вывод_массива(arr);
+            Console.ReadLine();
+
+        }
+    }
+}
